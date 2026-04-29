c662650afabca47d6e287140a26d0d54055847df
Stormy87 <tomek.jarzabek87@gmail.com>
2026-04-29 15:18:24 +0200

    Add models for Author, Publisher, Genre, Series, Topic, and Note; update Book model and settings

--- manage_books/models.py
+++ manage_books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import pytz
 
 # Create your models here.
 class Book(models.Model):
@@ -18,6 +19,8 @@ class Book(models.Model):
         ('other', 'Other'),
     ]
 
+
+
     title = models.CharField(max_length=200)
     isbn = models.CharField(max_length=20, unique=True)
     publication_date = models.DateField()
@@ -26,16 +29,53 @@ class Book(models.Model):
     language = models.CharField(max_length=20, choices=LANGUAGES)
     is_read = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
-# class Author(models.Model):
+    authors = models.ManyToManyField('Author', related_name='books', blank=True) 
+    publisher = models.ForeignKey('Publisher', on_delete=models.RESTRICT)
+    series = models.ForeignKey('Series', on_delete=models.RESTRICT, blank=True, null=True)
+    genres = models.ForeignKey('Genre', on_delete=models.RESTRICT, blank=True, null=True)
+    topics = models.ForeignKey('Topic', on_delete=models.RESTRICT, blank=True, null=True)
 
-# class Publisher(models.Model):
 
-# class Genre(models.Model):
 
-# class Series(models.Model):
+class Author(models.Model):
+    TITLES = [
+        ('ks', 'Ks.'),
+        ('dr', 'Dr.'),
+        ('prof', 'Prof.'),
+        ('bp', 'Bp.'),
+    ]
 
-# class Topic(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100, blank=True, null=True)
+    nationality = models.CharField(max_length=100)
+    title = models.CharField(max_length=50, choices=TITLES, blank=True, null=True)
+    series = models.ManyToManyField('Series', related_name='authors', blank=True)
 
-# class Category(models.Model):
 
-# class Note(models.Model):
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100, choices=pytz.country_names.items())
+    founded_year = models.IntegerField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+
+class Genre(models.Model): #gatunek (literacki)
+    name = models.CharField(max_length=100)
+
+
+class Series(models.Model): #seria (książek)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+
+
+class Topic(models.Model): #temat (książki)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+class Note(models.Model): 
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,)
